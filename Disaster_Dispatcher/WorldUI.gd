990f7f68488538089949c0f4fe807f21b106a612
terrainbuilder52 <32080764+terrainbuilder52@users.noreply.github.com>
2020-02-18 12:39:39 -0600

    Add ability to hide menu

--- Disaster_Dispatcher/WorldUI.gd
+++ Disaster_Dispatcher/WorldUI.gd
@@ -1,18 +1,12 @@
 extends Control
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var city_level = get_parent().get_node("City").get("city_level")
 	if city_level == 3:
 		$CanvasLayer/Panel/VSplitContainer/Button.text = "MAXED"
+			
